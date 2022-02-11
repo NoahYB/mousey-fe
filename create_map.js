40495424e2d3f1364a21b8294d49c396d83fe7b8
NoahYB <nybrinton@gmail.com>
2022-02-10 16:38:00 -0800

    increase wall width

--- create_map.js
+++ create_map.js
@@ -1,10 +1,10 @@
 
 function createMap(width, height) {
     let entities = [];
-    let ground = Bodies.rectangle(width - width / 2, height, width, 10, { isStatic: true });
-    entities.push(Bodies.rectangle(0, height - height / 2, 10, height, { isStatic: true }));
-    entities.push(Bodies.rectangle(width, height - height / 2, 10, height, { isStatic: true }));
-    entities.push(Bodies.rectangle(0 + width / 2, 0, width, 10, { isStatic: true }));
+    let ground = Bodies.rectangle(width - width / 2, height, width, 100, { isStatic: true });
+    entities.push(Bodies.rectangle(0, height - height / 2, 100, height, { isStatic: true }));
+    entities.push(Bodies.rectangle(width, height - height / 2, 100, height, { isStatic: true }));
+    entities.push(Bodies.rectangle(0 + width / 2, 0, width, 100, { isStatic: true }));
 
     entities.push(Bodies.rectangle(0, 300, width / 3, 10, { isStatic: true }));
     ground.label = 'ground';
